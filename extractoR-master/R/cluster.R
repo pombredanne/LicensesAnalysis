3a7650a627edb8b55e70b535070cd89be07a74bd
rmeloca <rmeloca@gmail.com>
2017-04-08 04:54:40 -0300

    storage analysis

--- extractoR-master/R/cluster.R
+++
@@ -1,9 +0,0 @@
-InitCluster <- function(logger, logfile, n=4) {
-  cl <- makeCluster(n, type="PSOCK", outfile="")
-  clusterExport(cl, list(logfile="logfile"), envir=environment())
-  clusterCall(cl, function() {
-    logging::basicConfig()
-    logging::addHandler(writeToFile, logger=logger, file=logfile)
-  })
-  cl
-}
