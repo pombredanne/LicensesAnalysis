e665ce832d3f94ce10acf51aa6a0e8ce97ff3849
rmeloca <rmeloca@gmail.com>
2017-05-18 22:38:26 -0300

    normalize dependency list and get irregular edges to match with list type and copyleft classes

--- npm/getDistribution.py
+++ npm/getDistribution.py
@@ -3,18 +3,19 @@ import json
 def getDistribution(packages):
 	distribution = {}
 	for package in packages:
-		try:
-			license = packages[package]["license"]
-			if license == None:
-				license = "None"
-			distribution[license] = distribution[license] + 1
-		except Exception as e:
-			distribution[license] = 1
+		licenseList = packages[package]["license"]
+		if licenseList == None:
+			licenseList = ["none"]
+		for license in licenseList:
+			try:
+				distribution[license] = distribution[license] + 1
+			except Exception as e:
+				distribution[license] = 1
 	return distribution
 
 if __name__ == '__main__':
-	with open("dependencyList.json") as dependencyList:
+	with open("normalizedDependencyList.json") as dependencyList:
 		packages = json.load(dependencyList)
 		distribution = getDistribution(packages)
-		with open("distribution.json", "w") as distributionList:
+		with open("normalizedDistribution.json", "w") as distributionList:
 			distributionList.write(json.dumps(distribution))
--- npm/getIrregularEdges.py
+++ npm/getIrregularEdges.py
@@ -1,62 +1,20 @@
 import json
 
 PACKAGES = {}
-PERMISSIVITY_INDEX = []
+COPYLEFT_LICENSES = []
 
-def getLicense(package):
+def getLicenseList(package):
 	return PACKAGES[package]["license"]
-
-def getPermissivityIndex(license):
-	if license != None:
-		license = license.upper()
-	for index in range(len(PERMISSIVITY_INDEX)):
-		if license in PERMISSIVITY_INDEX[index]:
-			return index
-	raise Exception
-	
-def insertLicenseToIndex(license, index):
-	if type(index) == str:
-		try:
-			index = int(index)
-		except ValueError as e:
-			index = float(index)
-			index = int(index)
-			PERMISSIVITY_INDEX.insert(index, [])
-	if index < 0:
-		index = 0
-		PERMISSIVITY_INDEX.insert(index, [])
-	elif index > len(PERMISSIVITY_INDEX):
-		index = len(PERMISSIVITY_INDEX)
-		PERMISSIVITY_INDEX.insert(index, [])
-	if license != None:
-		license = license.upper()
-	PERMISSIVITY_INDEX[index].append(license)
-	with open("permissivityIndex.json", "w") as permissivityIndex:
-		permissivityIndex.write(json.dumps(PERMISSIVITY_INDEX))
-
-def compareLicenses(license1, license2):
-	try:
-		license1_index = getPermissivityIndex(license1)
-	except Exception as e:
-		pass
-	try:
-		license2_index = getPermissivityIndex(license2)
-	except Exception as e:
-		pass
-	if license1_index == license2_index:
-		return 0
-	elif license1_index > license2_index:
-		return 1
-	elif license1_index < license2_index:
-		return -1
-	else:
-		raise Exception
 
 def isIrregularEdge(edge):
 	try:
-		license1 = getLicense(edge[0])
-		license2 = getLicense(edge[1])
-		return compareLicenses(license1, license2) > 0
+		origemLicenseList = getLicenseList(edge[0])
+		destinoLicense = getLicenseList(edge[1])
+		for origem in origemLicenseList:
+			for destino in destinoLicense:
+				if origem not in COPYLEFT_LICENSES and destino in COPYLEFT_LICENSES:
+					return True
+		return False
 	except Exception as e:
 		return True
 
@@ -67,13 +25,13 @@ def printIrregularEdges():
 			edge = (package, dependency)
 			if isIrregularEdge(edge):
 				try:
-					print(edge[0], getLicense(package), " --> ", edge[1], getLicense(dependency))
+					print(edge[0], getLicenseList(package), " --> ", edge[1], getLicenseList(dependency))
 				except Exception as e:
 					pass
 
 if __name__ == '__main__':
-	with open("dependencyList.json") as dependencyList:
+	with open("normalizedDependencyList.json") as dependencyList:
 		PACKAGES = json.load(dependencyList)
 		with open("permissivityIndex.json") as permissivityIndex:
-			PERMISSIVITY_INDEX = json.load(permissivityIndex)
+			COPYLEFT_LICENSES = json.load(permissivityIndex)
 		printIrregularEdges()
