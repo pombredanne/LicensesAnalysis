1bf0cf8e3fe4fb39d6863c8e22d1105ceb3a22dc
rmeloca <rmeloca@gmail.com>
2017-06-01 13:47:13 -0300

    fix and improve

--- npm/calculateGlobalRegularityRate.py
+++ npm/calculateGlobalRegularityRate.py
@@ -6,28 +6,29 @@ GLOBAL_REGULARITY_RATE = {}
 VISITED_PACKAGES = []
 
 def calculateTree(package):
-	if package in VISITED_PACKAGES:
-		return
 	VISITED_PACKAGES.append(package)
 	globalRegularityRate = PACKAGES[package]["regularityRate"]
 	dependencies = PACKAGES[package]["dependencies"]
 	for dependency in dependencies:
 		try:
 			dependencyName = dependency["package"]
-			if dependencyName not in GLOBAL_REGULARITY_RATE.keys() and dependencyName not in VISITED_PACKAGES:
+			if dependencyName not in VISITED_PACKAGES:
 				calculateTree(dependencyName)
 			globalRegularityRate *= GLOBAL_REGULARITY_RATE[dependencyName]
-			if GLOBAL_REGULARITY_RATE[dependencyName] < 1:
-				print("[" + str(len(VISITED_PACKAGES)) + "/" + str(len(PACKAGES.keys())) + "]", '\033[1m' + dependencyName + '\033[0m', "\t", "{" + str(len(PACKAGES[dependencyName]["dependencies"])) + "}", "\t", PACKAGES[dependencyName]["regularityRate"], "->", GLOBAL_REGULARITY_RATE[dependencyName])
 		except Exception as e:
 			pass
 	GLOBAL_REGULARITY_RATE[package] = globalRegularityRate
 
 def calculateForest():
 	for package in PACKAGES:
+		method = "M:"
 		if package not in GLOBAL_REGULARITY_RATE.keys():
 			calculateTree(package)
+			method = "C:"
 		PACKAGES[package]["globalRegularityRate"] = GLOBAL_REGULARITY_RATE[package]
+		if GLOBAL_REGULARITY_RATE[package] < 1:
+			print(method, end=" ")
+			print("[" + str(len(VISITED_PACKAGES)) + "/" + str(len(PACKAGES.keys())) + "]", '\033[1m' + package + '\033[0m', "\t", "{" + str(len(PACKAGES[package]["dependencies"])) + "}", "\t", PACKAGES[package]["regularityRate"], "->", GLOBAL_REGULARITY_RATE[package])
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
